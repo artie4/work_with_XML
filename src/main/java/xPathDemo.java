0c8821db8b727993409aae015ba1100ef2b38d0a
Arthur <gphone1125@gmail.com>
2018-05-06 22:50:36 +0300

    first

---
+++ src/main/java/xPathDemo.java
@@ -0,0 +1,45 @@
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.IOException;
+
+public class xPathDemo {
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("E:\\Projects\\JavaServerWebApp\\parseXML\\exampleParseXML\\src\\main\\resources\\usage.statistics.xml"));
+
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xpath = xpf.newXPath();
+
+//        E:\Projects\JavaServerWebApp\parseXML\exampleParseXML\src\main\resources\books.xml
+//        /catalog/book
+//        //child::*
+//        count(//book[@id='bk101']/child::*)
+//        count(//book[@id]/child::*)
+//        count(//book[@id='bk101']/price/ancestor::*)
+//        price='49.9'
+//        descendant::book
+//        preceding-sibling:://book[1]/price
+//        generate-id(//book)
+
+//        E:\Projects\JavaServerWebApp\parseXML\exampleParseXML\src\main\resources\pusage.statistics.xml
+//        count(/application/component[@name='ActionsCollector']/counts/child::*)
+
+
+        System.out.println(xpath.evaluate("//component[@name='ActionsCollector']/counts/entry[1]/@action", document));
+
+
+//        System.out.println(xpath.evaluate("", document, XPathConstants.NODESET));
+//        System.out.println(xpath.evaluate("", document, XPathConstants.NODE));
+//        System.out.println(((Number) xpath.evaluate("", document, XPathConstants.NUMBER)).intValue());
+
+    }
+}
